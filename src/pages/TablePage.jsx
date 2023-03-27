179a0105b33fed0ab6dccdfe55460ca52fd0f22b
yuridapaz <yuridapaz@gmail.com>
2023-03-27 19:53:31 -0300

    up

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import PaymentBottomCard from '../components/PaymentBottomCard';
